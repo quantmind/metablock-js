541096831599d31489bb19f48c6212dc75a87738
Luca Sbardella <luca@quantmind.com>
2020-07-06 19:33:26 +0100

    Fix some server issues

--- packages/metablock-server/src/html/html.ts
+++ packages/metablock-server/src/html/html.ts
@@ -6,14 +6,20 @@ export default (ctx: Context, req: Request) => {
   // const csrfToken = req.csrfToken();
   //const init = JSON.stringify({ ...web, csrfToken });
   const init = JSON.stringify(ctx.web);
+  const title = ctx.web.title || ctx.web.name;
+  const description = ctx.web.description || title;
   const scripts = [
     `<script type="application/javascript">__metablock_assets_url__="${ctx.web.assetsUrl}/";</script>`,
   ].concat(
     ctx.html.scripts.map((script) => `<script src="${script}"></script>`)
   );
-  const head = ctx.html.meta
-    .concat([`<meta name="mb:state" content="${btoa(init)}">`])
-    .concat(ctx.html.css);
+  const head = [
+    `<title>${title}</title>`,
+    `<meta name="description" content="${description}">`,
+    `<meta name="mb:state" content="${btoa(init)}">`,
+    ...ctx.html.meta,
+    ...ctx.html.css,
+  ];
   const afterBody = scripts.concat(ctx.html.afterBody);
 
   return `<!doctype html>
--- packages/metablock-server/src/html/statusCode.ts
+++ packages/metablock-server/src/html/statusCode.ts
@@ -1,9 +1,13 @@
 import { Page } from "puppeteer";
 
 export default async (page: Page): Promise<any> => {
-  const statusCode = await page.$eval("meta[name='page-status-code']", (el) =>
-    //@ts-ignore
-    el ? +el.content : null
-  );
-  return statusCode ? { statusCode } : null;
+  try {
+    const statusCode = await page.$eval("meta[name='page-status-code']", (el) =>
+      //@ts-ignore
+      el ? +el.content : null
+    );
+    return statusCode ? { statusCode } : null;
+  } catch (exc) {
+    return;
+  }
 };
