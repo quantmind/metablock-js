26925f01ac9e577c0e80a08ca151bd70829c0074
Luca <luca@b2c2.com>
2020-11-28 15:16:55 +0000

    Release 0.7.0

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from "commander";
+import process from "process";
 import { version } from "../package.json";
 import { publish } from "./publish";
 import { devVersion, updateVersion } from "./version";
@@ -8,19 +9,24 @@ const program = new Command();
 program.version(version);
 program.arguments("[cmd]").action((cmd) => program.help());
 
+const error = (e: any) => {
+  process.exitCode = 1;
+  console.error(e.message || e);
+};
+
 program
   .command("dev")
   .description("Set dev dependencies - for development")
-  .action(() => devVersion());
+  .action(() => devVersion().catch(error));
 
 program
   .command("update")
   .description("Update version of packages")
-  .action(() => updateVersion());
+  .action(() => updateVersion().catch(error));
 
 program
   .command("publish")
   .description("Publish to npm")
-  .action(() => publish());
+  .action(() => publish().catch(error));
 
 program.parse(process.argv);
--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,11 +1,10 @@
 import chalk from "chalk";
 import { exec } from "child-process-promise";
 import logger from "console";
-import { getPackagePath, getPackages, updateVersion } from "./version";
+import { getPackagePath, getPackages } from "./version";
 
 export const publish = async () => {
   const dirs = getPackages();
-  await updateVersion();
   for (let i = 0; i < dirs.length; ++i) {
     const packagePath = getPackagePath(dirs[i]);
     logger.log(chalk.green(`   npm publish --access public ${packagePath}`));
