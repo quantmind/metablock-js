0fdcd8c1e179b1556c4fd438d1d3eaa39185f47f
Luca <luca@b2c2.com>
2020-11-29 11:02:55 +0000

    fix deploymet

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -3,11 +3,18 @@ import { exec } from "child-process-promise";
 import logger from "console";
 import { getPackagePath, getPackages } from "./version";
 
+const publishCommand = "npm publish --access public";
+const command = [
+  "echo '//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}' > .npmrc",
+  publishCommand,
+  "rm .npmrc",
+].join(" && ");
+
 export const publish = async () => {
   const dirs = getPackages();
   for (let i = 0; i < dirs.length; ++i) {
     const packagePath = getPackagePath(dirs[i]);
-    logger.log(chalk.green(`   npm publish --access public ${packagePath}`));
-    await exec("npm publish --access public", { cwd: packagePath });
+    logger.log(chalk.green(`   ${publishCommand} ${packagePath}`));
+    await exec(command, { cwd: packagePath });
   }
 };
