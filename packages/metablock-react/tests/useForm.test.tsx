16944260b0fe8b1aad51bd9e77df8a8fe303da02
Luca <luca@b2c2.com>
2021-04-09 23:56:08 +0100

    Release 0.9.2

--- packages/metablock-react/tests/useForm.test.tsx
+++ packages/metablock-react/tests/useForm.test.tsx
@@ -21,4 +21,22 @@ describe("useForm", () => {
     expect(submits.data).toEqual({});
     expect(submits.dirty).toEqual({});
   });
+
+  test("setValue", async () => {
+    const submits: any = {};
+    const handleSubmit = async (data: any, dirty: any) => {
+      submits.data = data;
+      submits.dirty = dirty;
+    };
+    const { result } = renderHook(() => useForm({ handleSubmit }));
+    expect(result.current.data).toEqual({});
+    const onSubmit = result.current.onSubmit();
+
+    result.current.setValue("test", "this is a test");
+
+    await onSubmit();
+
+    expect(submits.data).toEqual({ test: "this is a test" });
+    expect(submits.dirty).toEqual({ test: "this is a test" });
+  });
 });
