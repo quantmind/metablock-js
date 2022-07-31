582af5b6be3665c019bca475c9c7d7e50b128e4c
Luca Sbardella <luca@quantmind.com>
2022-07-31 15:15:26 +0100

    Release 0.18.6

--- packages/metablock-react/src/components/Header.tsx
+++ packages/metablock-react/src/components/Header.tsx
@@ -1,7 +1,6 @@
 import Menu from "@mui/icons-material/Menu";
 import { Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Drawer from "@mui/material/Drawer";
