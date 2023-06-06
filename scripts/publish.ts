1dcba81e4b578897093e96b58b567f473640d0f9
Luca Sbardella <luca@quantmind.com>
2023-06-06 08:43:55 +0100

    release

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -3,17 +3,12 @@ import logger from "console";
 import { getPackagePath, getPackages } from "./version";
 
 const publishCommand = "npm publish --access public";
-const command = [
-  "echo '//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}' > .npmrc",
-  publishCommand,
-  "rm .npmrc",
-].join(" && ");
 
 export const publish = async () => {
   const dirs = getPackages();
   for (let i = 0; i < dirs.length; ++i) {
     const packagePath = getPackagePath(dirs[i]);
     logger.log(`   ${publishCommand} ${packagePath}`);
-    await exec(command, { cwd: packagePath });
+    await exec(publishCommand, { cwd: packagePath });
   }
 };
